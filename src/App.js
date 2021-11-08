31719c62792eb079bb014cc83b3ff61181320e33
Marius Kuzminskas <marius.kuzminskas@ca.com>
2021-11-08 11:27:56 +0200

    styled props

--- src/App.js
+++ src/App.js
@@ -1,12 +1,28 @@
 import './App.css';
+// import Button from './components/ui/Button';
 // import Services from './components/services/Services';
-import Plan from './components/Plan';
+// import Plan from './components/Plan';
+import styled from 'styled-components';
+
+const MyButton = styled.button`
+  font-size: 3rem;
+  padding: 10px;
+
+  background-color: ${(props) => (props.extra ? 'tomato' : 'white')};
+`;
+
+function Button(props) {
+  // logic
+  return <MyButton extra={props.extra}>{props.children}</MyButton>;
+}
 
 function App() {
   return (
     <div className='App'>
       {/* <Services /> */}
-      <Plan />
+      {/* <Plan /> */}
+      <Button> Normal button</Button>
+      <Button extra> Extra button</Button>
     </div>
   );
 }
